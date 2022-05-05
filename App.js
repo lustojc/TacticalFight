4e8e5d39af17086a318b677f337d25cc57e88f9b
lustojc <sashalusto228@gmail.com>
2022-05-05 21:02:32 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View>
+      <Text>123</Text>
+    </View>
+  );
+};
+
+export default App;
